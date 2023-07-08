ec736d0e24041878867749d9d4beea555c9ae7ac
“emilioantonio29” <emilio_git@outlook.com>
2023-07-08 20:38:16 -0300

    Interpolation | Expressions | View Filters [pipes] | Directives [Atributes (ngClass & ngStyle), Custom, structure (ngFor), conditionals (ngIf & ngSwitch), Binding (Template Expressions, Template Statements & Property Binding)]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-project';
+  number = 1;
+  hayError = true;
+  myFontSize = '50px';
+  arrayStudents = ['Emi2', 'Zun2', 'Jimmy2']
+  status = 1;
+  value = "";
+  usuario = {
+    name: "DefaultName",
+    lastname: "DefaultLastname"
+  }
+  birthday= new Date();
+  price = 2500.23;
+
+  onSave = () =>{
+    console.log(this.usuario)
+  }
+
+  changeValue = (data: string) =>{
+    this.value = data;
+  }
+
+  changeError = () =>{
+    this.hayError = !this.hayError;
+  }
+
+  onClick = (event: MouseEvent): void =>{
+    console.log(event);
+  }
+
+  onInputChange = (event: Event): void =>{
+    console.log(event);
+    let html = event.target as HTMLInputElement;
+    this.changeValue(html.toString());
+  }
+
+  constructor(){
+    console.log("test")
+    setInterval(()=>{
+      this.number++;
+    },1000)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { GeneralComponentComponent } from './general-component/general-component.component';
-import { MainWrapperComponent } from './main-wrapper/main-wrapper.component';
-import { StyledTextComponent } from './styled-text/styled-text.component';
+import { RoundedBlockDirective } from './directives/rounded/rounded-block.directive';
+import { DraggableDirective } from './directives/draggable/draggable.directive';
+import { FormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+// import { HeaderComponent } from './header/header.component';
+// import { FooterComponent } from './footer/footer.component';
+// import { GeneralComponentComponent } from './general-component/general-component.component';
+// import { MainWrapperComponent } from './main-wrapper/main-wrapper.component';
+// import { StyledTextComponent } from './styled-text/styled-text.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    GeneralComponentComponent,
-    MainWrapperComponent,
-    StyledTextComponent,
+    RoundedBlockDirective,
+    DraggableDirective,
+    // HeaderComponent,
+    // FooterComponent,
+    // GeneralComponentComponent,
+    // MainWrapperComponent,
+    // StyledTextComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
