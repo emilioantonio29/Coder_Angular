f0caf973338ffd89d5a7b795aee8023906513771
“emilioantonio29” <emilio_git@outlook.com>
2023-07-09 17:59:10 -0300

    angularTemplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { GeneralComponentComponent } from './general-component/general-component.component';
-import { MainWrapperComponent } from './main-wrapper/main-wrapper.component';
-import { StyledTextComponent } from './styled-text/styled-text.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    GeneralComponentComponent,
-    MainWrapperComponent,
-    StyledTextComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
